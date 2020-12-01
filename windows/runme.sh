8843d9029cbc888bab8fb2cba7a8377e109d6a7f
jay vyas <jay@apache.org>
2020-12-01 16:43:51 -0500

    Update runme w/ image replacement and ova uploads

--- windows/runme.sh
+++ windows/runme.sh
@@ -1,6 +1,17 @@
 source ~/env.sh
+
+# wget https://build-artifactory.eng.vmware.com/artifactory/webapp/#/artifacts/browse/tree/General/k8simages-windows-local/windows-2019-kube-v1.19.1-docker.ova
+#govc import.ova https://build-artifactory.eng.vmware.com/k8simages-windows-local/windows-2019-kube-v1.19.1-docker.ova
+
+## wget peri-min.yaml.sh
+
 cat peri-min.yaml.sh | sed s/\$VSPHERE_USERNAME/administrator@vsphere.local/g > peri-min.yaml
+
+# Set image to run gabi's capv controller
+kubectl set image deployment/capv-controller-manager manager=harbor-repo.vmware.com/tkgwindows/vsphere-manager:dev
+
 echo "x"
+
 
 sed -i s/\$VSPHERE_PASSWORD/$VSPHERE_PASSWORD/  peri-min.yaml
 sed -i s/\$VSPHERE_DATACENTER/$VSPHERE_DATACENTER/  peri-min.yaml
@@ -12,13 +23,14 @@ sed -i s/\$VSPHERE_CP_IP/$VSPHERE_CP_IP/  peri-min.yaml
 KEYYYY=$(printf %q "$VSPHERE_SSH_AUTHORIZED_KEY")
 echo $KEYYYY 
 
-
 sed -i 's/\$VSPHERE_SSH_AUTHORIZED_KEY/\"ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6VfBKd6hqd5h7k5f+AtjJSV1hdW5u9\/3uAolK3SD2\/5GD9+rn+FMSdbtkeaKuuVJPi2HjnsVMO+r8WcuyN5ZSYHywiSoh4S7PamAxra1CLISsFHPYFlGrtdHC70wnoT7+\/wAJk2D3CYkCNMWIxs5eR0cefDOytipBfDplhkJByyrcnXuhI8St3XJzpjlXu454diJOxfsk6axanWLOr\/WZFmUi1U6V4gRE7XtKG9WFUm1bmNgkgd7lehKzi+isTjnI+b4tnD0yIzKFcsgIvLdGJTI6Lluj33CeBHIocwu0LbvowTyYSqhP6DzGhGuKfK9rMnJh\/ll0Bnu1xf\/ok0NSQ== tkg\"/' peri-min.yaml 
 
 sed -i s/\$VSPHERE_DATASTORE/$VSPHERE_DATASTORE/  peri-min.yaml 
 
 echo "done substituting !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
-kubectl apply -f vspheremachinetemplates.crd.yaml
-kubectl create -f peri-min.yaml
+#peri-min.yaml
 
+kubectl delete -f peri-min.yaml
+sleep 60
+kubectl create -f peri-min.yaml

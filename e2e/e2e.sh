4cfab431eea48c42ee65cb0db7b9627e4eb515d9
jay vyas <jvyas@vmware.com>
2021-05-17 13:20:41 -0700

    Update e2e.sh

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -6,8 +6,8 @@ cp /tmp/e2e.test ./e2e.test
 chmod 755 ./e2e.test
 echo "e2e.test is ready to run now"
 ./e2e.test --version
-./e2e.test --provider=local --kubeconfig=/home/kubo/.kube/config \
---dump-logs-on-failure=false \
---ginkgo.focus='should be able to create a functioning NodePort service for Windows' \
---ginkgo.skip='Slow' \
---node-os-distro=windows
+#./e2e.test --provider=local --kubeconfig=/home/kubo/.kube/config \
+#--dump-logs-on-failure=false \
+#--ginkgo.focus='should be able to create a functioning NodePort service for Windows' \
+#--ginkgo.skip='Slow' \
+#--node-os-distro=windows
